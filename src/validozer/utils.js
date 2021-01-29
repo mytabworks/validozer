2f1e27aa7fc51d85a8bb146e14b7b45a8c744b25
mytabworks <mytabworks@gmail.com>
2021-01-29 16:24:39 +0800

    ✨ add num & und

--- src/validozer/utils.js
+++ src/validozer/utils.js
@@ -1,4 +1,6 @@
 export const is = {
     obj: (a) => !!a && a !== '[object Object]' && a.toString() === '[object Object]',
-    fnc: (a) => typeof a === 'function'
+    fnc: (a) => typeof a === 'function',
+    und: (a) => typeof a === 'undefined',
+    num: (a) => !isNaN(a) && typeof a === 'number',
 }
